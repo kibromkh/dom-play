b862be90e3b20891f6a8584d2a1e19702c92bad2
Kibrom Hagos <royalkibrom@gmail.com>
2023-06-28 09:45:45 -0700

    update

---
+++ js/dom-play.js
@@ -0,0 +1,15 @@
+//alert("HI from DOM Play");
+
+/*
+
+    With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+    
+    Un-highlight (normal or otherwise) all lines from all other actors in the play
+    
+    must accommodate any number of actors, and any number of additional lines
+    
+    We can chanhge the HTML as needs
+
+*/
+
+//document.querySelector(".hamlet").style.backgroundColor = "yellow";
